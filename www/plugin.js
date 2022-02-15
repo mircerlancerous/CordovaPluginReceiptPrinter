d744f3c61cdf92a3957016f4770176228b2f9dcb
mircerlancerous <mircerlancerous@users.noreply.github.com>
2022-02-15 13:38:10 -0800

    Update plugin.js

--- www/plugin.js
+++ www/plugin.js
@@ -1,33 +1,33 @@
 Plugin.receiptPrinter = {
 	open: function(callback, onFail){
-		cordova.exec(callback, onFail, 'SGT88iVPrinterPlugin', 'connect', []);
+		cordova.exec(callback, onFail, 'ReceiptPrinter', 'connect', []);
 	},
 
 	close: function(callback, onFail){
-		cordova.exec(callback, onFail, 'SGT88iVPrinterPlugin', 'disconnect', []);
+		cordova.exec(callback, onFail, 'ReceiptPrinter', 'disconnect', []);
 	},
 
 	isOpen: function(callback, onFail){
-		cordova.exec(callback, onFail, 'SGT88iVPrinterPlugin', 'isConnected', []);
+		cordova.exec(callback, onFail, 'ReceiptPrinter', 'isConnected', []);
 	},
 	
 	printText: function(callback, onFail, value){
-		cordova.exec(callback, onFail, 'SGT88iVPrinterPlugin', 'printText', [value]);
+		cordova.exec(callback, onFail, 'ReceiptPrinter', 'printText', [value]);
 	},
 	
 	command: function(callback, onFail, value){
-		cordova.exec(callback, onFail, 'SGT88iVPrinterPlugin', 'command', [value]);
+		cordova.exec(callback, onFail, 'ReceiptPrinter', 'command', [value]);
 	},
 
 	printBarcode: function(callback, onFail, type, value){
-		cordova.exec(callback, onFail, 'SGT88iVPrinterPlugin', 'printBarcode', [type, value]);
+		cordova.exec(callback, onFail, 'ReceiptPrinter', 'printBarcode', [type, value]);
 	},
 
 	cutPaper: function(callback, onFail){
-		cordova.exec(callback, onFail, 'SGT88iVPrinterPlugin', 'cutPaper', []);
+		cordova.exec(callback, onFail, 'ReceiptPrinter', 'cutPaper', []);
 	},
 
 	openCashBox: function(callback, onFail){
-		cordova.exec(callback, onFail, 'SGT88iVPrinterPlugin', 'openCashBox', []);
+		cordova.exec(callback, onFail, 'ReceiptPrinter', 'openCashBox', []);
 	}
 };
